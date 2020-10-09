e299841625cef1079ce566cec5a844768ac71392
Sergey Sintsov <sin@MacBook-Pro-Sergey.local>
2020-10-10 00:33:34 +0300

    game viselitsa

---
+++ lib/game.rb
@@ -0,0 +1,65 @@
+class Game
+  TOTAL_ERRORS_ALLOWED = 7
+
+  def initialize(word)
+    @letters = word.chars
+    @user_guesses = []
+  end
+
+  def errors
+    @user_guesses - normalized_letters
+  end
+
+  def errors_made
+    errors.length
+  end
+
+  def errors_allowed
+    TOTAL_ERRORS_ALLOWED - errors_made
+  end
+
+  def normalize_letter(letter)
+    case letter
+    when "Е" then "Ё"
+    when "И" then "Й"
+    else
+    letter
+    end
+  end
+
+  def normalized_letters
+    @letters.map { |letter| normalize_letter(letter) }
+  end
+
+  def letters_to_guess
+    @letters.map do |letter|
+      if @user_guesses.include?(normalize_letter(letter))
+        letter
+      end
+    end
+  end
+
+  def lost?
+    errors_allowed == 0
+  end
+
+  def over?
+    won? || lost?
+  end
+
+  def play!(letter)
+    normalized_letter = normalize_letter(letter)
+
+    if !over? && !@user_guesses.include?(normalized_letter)
+      @user_guesses << normalized_letter
+    end
+  end
+
+  def won?
+    (normalized_letters - @user_guesses).empty?
+  end
+
+  def word
+    @letters.join
+  end
+end
